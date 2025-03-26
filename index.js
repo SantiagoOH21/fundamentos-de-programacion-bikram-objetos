09166f234f66b42d9efea02736cd5e5addab12eb
SantiagoOH21 <soh0621@gmail.com>
2025-03-26 20:34:08 +0100

    final

--- index.js
+++ index.js
@@ -1,103 +1,114 @@
+//DECLARACIÓN
 const Coche = {
-    marca: "Ferrari",
-    modelo: "Testarrosa",
-    matricula: 123456789,
-}
+  marca: "Ferrari",
+  modelo: "Testarrosa",
+  matricula: 123456789,
+};
 
 const Casa = {
-    codPostal: "40025",
-    calle: "Laurin",
-    portal: "14",
-    piso: "4"
-}
+  codPostal: "40025",
+  calle: "Laurin",
+  portal: "14",
+  piso: "4",
+};
 
 const FullStackDeveloper = {
-    lenguajes: ["JavaScript", "Python", "C#"],
-    proyectos: ["Angle", "Neural Interface Technologies"],
-}
+  lenguajes: ["JavaScript", "Python", "C#"],
+  proyectos: ["Angle", "Neural Interface Technologies"],
+};
 
 const Perro = {
-    nombre: "Puppy",
-    raza: "Pitbull",
-    color: "Negro",
-    edad: 5,
+  nombre: "Puppy",
+  raza: "Pitbull",
+  color: "Negro",
+  edad: 5,
 
-    ladrar: function() {
-        console.log("ladrar")
-    },
+  ladrar: function () {
+    console.log("ladrar");
+  },
 
-    popo: function() {
-        return Math.random() * 3
-    }
-}
+  popo: function () {
+    return Math.random() * 3;
+  },
+};
 
-const marcaPortatil = Portatil.marca
+//LECTURA DE PROPIEDADES
+const marcaPortatil = Portatil.marca;
 
-const marcaPortatil2 = Portatil["marca"]
+const marcaPortatil2 = Portatil["marca"];
 
-const grupos = Concierto.grupos 
+const grupos = Concierto.grupos;
 
-const RGB = [Led.rojo, Led.verde, Led.azul]
+const RGB = [Led.rojo, Led.verde, Led.azul];
 
-Portatil.modelo = "P345"
+//MODIFICACIÓN DE PROPIEDADES
+Portatil.modelo = "P345";
 
-Concierto.cartelera.push("Guns N' Roses")
+Concierto.cartelera.push("Guns N' Roses");
 
-Concierto.fecha = new Date()
+Concierto.fecha = new Date();
 
-Impresora.imprimiendo = {nombreArchivo: "1", copias: "2",  numPaginas: 13}
+Impresora.imprimiendo = { nombreArchivo: "1", copias: "2", numPaginas: 13 };
 
+//DECLARACIÓN
 const Noticia = {
-    titular: "Ai Revolution",
-    cuerpo: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae corrupti vel cum?"
-}
+  titular: "Ai Revolution",
+  cuerpo:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae corrupti vel cum?",
+};
 
 const Persona = {
-    nombre: "John",
-    apellidos: "Anderson",
-    edad: 25
-}
+  nombre: "John",
+  apellidos: "Anderson",
+  edad: 25,
+};
 
 const Avion = {
-    numPasajeros: 235,
+  numPasajeros: 235,
 
-    despegar: function() {
-        console.log("despegando")
-    },
+  despegar: function () {
+    console.log("despegando");
+  },
 
-    volar: function() {
-        console.log("llegando al destino")
-    },
+  volar: function () {
+    console.log("llegando al destino");
+  },
 
-    aterrizar: function() {
-        console.log("aterrizando")
-    }
-}
+  aterrizar: function () {
+    console.log("aterrizando");
+  },
+};
 
 const Paquete = {
-    contenido: [1, 2, 3]
-}
+  contenido: [1, 2, 3],
+};
 
 const Pais = {
-    numHabitantes: 400000,
-    continente: "Asia",
-    gentilicio: "Singapurense"
-}
+  numHabitantes: 400000,
+  continente: "Asia",
+  gentilicio: "Singapurense",
+};
 
-const codError = O_Error.codigo
+//LECTURA DE PROPIEDADES
+const codError = O_Error.codigo;
 
-const integrantes = Grupo.integrantes
+const integrantes = Grupo.integrantes;
 
-const nivelesTinta = {rojo: Impresora.tinta.rojo, verde: Impresora.tinta.verde, azul: Impresora.tinta.azul}
+const nivelesTinta = {
+  rojo: Impresora.tinta.rojo,
+  verde: Impresora.tinta.verde,
+  azul: Impresora.tinta.azul,
+};
 
-const pixeles = Pantalla.pixeles
+const pixeles = Pantalla.pixeles;
 
-const especificaciones = Movil["especificaciones"]
+const especificaciones = Movil["especificaciones"];
 
-Grupo.numIntegrantes = 5
+//MODIFICACIÓN DE PROPIEDADES
+Grupo.numIntegrantes = 5;
 
-Pantalla.dimensiones = "1920x1080"
+Pantalla.dimensiones = "1920x1080";
 
-Led.encendido = !Led.encendido
+Led.encendido = !Led.encendido;
 
-Movil.temperatura = "20º"
+Movil.temperatura = "20º";
